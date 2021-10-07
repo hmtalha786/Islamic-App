5f8ad6b1bd9ae3176ce22df1a3b0c8652dc1e9ac
Muhammad Talha <talhaofficialwork@gmail.com>
2021-10-07 17:29:34 +0500

    Footer Design Added

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { NativeBaseProvider } from "native-base";
+import {
+  NativeBaseProvider,
+  Text,
+  HStack,
+  Center,
+  Spacer,
+  Pressable,
+} from "native-base";
+import { Ionicons, FontAwesome5, FontAwesome } from "@expo/vector-icons";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Dashboard from "./files/Dashboard";
@@ -13,9 +21,11 @@ import Ziker from "./files/Ziker";
 import Others from "./files/Others";
 
 const Drawer = createDrawerNavigator();
+
 function App() {
   return (
     <>
+      {/* Header Bar and App Drawer Navigation */}
       <NavigationContainer>
         <Drawer.Navigator initialRouteName="Dashboard">
           <Drawer.Screen
@@ -138,6 +148,58 @@ function App() {
           />
         </Drawer.Navigator>
       </NavigationContainer>
+
+      {/* Footer Design Start From Here */}
+
+      <HStack pb="2.5" pt="1.5" alignItems="center" backgroundColor="#005db4">
+        <Spacer />
+        <Pressable cursor="pointer" py="3" flex={1}>
+          <Center>
+            <FontAwesome5 name="home" size={24} color="white" />
+            <Text color="white" fontSize="12">
+              Home
+            </Text>
+          </Center>
+        </Pressable>
+        <Spacer />
+        <Pressable cursor="pointer" py="3" flex={1}>
+          <Center>
+            <FontAwesome name="book" size={24} color="white" />
+            <Text color="white" fontSize="12">
+              Quran
+            </Text>
+          </Center>
+        </Pressable>
+        <Spacer />
+
+        <Pressable cursor="pointer" py="3" flex={1}>
+          <Center>
+            <FontAwesome5 name="mosque" size={24} color="white" />
+            <Text color="white" fontSize="12">
+              Prayer
+            </Text>
+          </Center>
+        </Pressable>
+        <Spacer />
+        <Pressable cursor="pointer" py="3" flex={1}>
+          <Center>
+            <FontAwesome5 name="praying-hands" size={24} color="white" />
+            <Text color="white" fontSize="12">
+              Ziker
+            </Text>
+          </Center>
+        </Pressable>
+        <Spacer />
+        <Pressable cursor="pointer" py="3" flex={1}>
+          <Center>
+            <Ionicons name="ios-apps" size={24} color="white" />
+            <Text color="white" fontSize="12">
+              More
+            </Text>
+          </Center>
+        </Pressable>
+        <Spacer />
+      </HStack>
     </>
   );
 }
--- files/Others.js
+++ files/Others.js
@@ -19,12 +19,57 @@
 // Settings
 
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import {
+  IconButton,
+  SimpleGrid,
+  Icon,
+  NativeBaseProvider,
+  Box,
+  Text
+} from "native-base";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function Others() {
+function Other() {
+
+  return (
+    <SimpleGrid columns={5} spacingY={8} spacingX={4}>
+      <Box alignItems="center">
+        <FontAwesome5 name="compass" size={24} color="black" />
+        <Text>Qibla</Text>
+      </Box>
+      <Box alignItems="center">
+        <AntDesign name="book" size={24} color="black" />
+        <Text>Quran</Text>
+      </Box>
+      <Box alignItems="center">
+        <MaterialIcons name="date-range" size={24} color="black" />
+        <Text>Ramadan Calander</Text>
+      </Box>
+      <Box alignItems="center">
+        <AntDesign name="setting" size={24} color="black" />
+        <Text>Settings</Text>
+      </Box>
+      <Box alignItems="center">
+        <FontAwesome5 name="compass" size={24} color="black" />
+        <Text>Hello</Text>
+      </Box>
+      <Box alignItems="center">
+        <FontAwesome5 name="compass" size={24} color="black" />
+        <Text>Hello</Text>
+      </Box>
+    </SimpleGrid>
+  );
+}
+
+export default function () {
   return (
     <NativeBaseProvider>
-      <Box>Others</Box>
+      <Box alignItems="center" justifyContent="space-between" pt="8" flex={1}>
+        <Other />
+      </Box>
     </NativeBaseProvider>
   );
 }
+
